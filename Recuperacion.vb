c11b0998fde78cbf6e7a364d8533a7b203fed15b
rony.rodriguez.sv <rony.rodriguez.sv@gmail.com>
2022-04-20 22:36:01 -0600

    recuperar

--- Recuperacion.vb
+++ Recuperacion.vb
@@ -5,5 +5,10 @@
 
     Private Sub Recuperacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Login.Visible = False
+        abrir()
+    End Sub
+
+    Private Sub btnacceder_Click(sender As Object, e As EventArgs) Handles btnacceder.Click
+
     End Sub
 End Class
